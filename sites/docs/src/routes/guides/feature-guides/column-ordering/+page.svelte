555ce903984619e7e8b848c1b0bf43a3d7907562
Sebastian Drozd <163430081+tzezar@users.noreply.github.com>
2024-10-12 18:47:01 +0200

    add column ordering doc

--- sites/docs/src/routes/guides/feature-guides/column-ordering/+page.svelte
+++ sites/docs/src/routes/guides/feature-guides/column-ordering/+page.svelte
@@ -1,6 +1,35 @@
 <script>
-
-	import PageNotDone from "$lib/blocks/page-not-done.svelte";
-
+	import PageCodeContainer from '$lib/blocks/page-code-container.svelte';
+	import PageContainer from '$lib/blocks/page-container.svelte';
+	import PageTitle from '$lib/blocks/page-title.svelte';
+	import { Highlight } from 'svelte-highlight';
+	import { typescript } from 'svelte-highlight/languages';
+	import { code } from './_components/code';
+	import ImportantInformation from '$lib/blocks/important-information.svelte';
+	import CH from '$lib/blocks/code-highlight.svelte';
+	import Datagrid from './_components/datagrid.svelte';
 </script>
-<PageNotDone />
+
+<PageContainer>
+	<PageTitle title="Column Ordering" />
+
+	<p>
+		On this page, you'll learn how to enable column reodering and see how it's implemented so you
+		can make your own enhancements.
+	</p>
+	<p>
+		Property <CH>moveable</CH> specifies whether a column can be reordered.
+	</p>
+
+	<PageCodeContainer>
+		<Highlight language={typescript} code={code.baseColumns} />
+	</PageCodeContainer>
+
+	<Datagrid />
+
+	<PageCodeContainer>
+		<Highlight language={typescript} code={code.columns} />
+	</PageCodeContainer>
+
+	<p><CH>grow</CH> and <CH>align</CH> was added for nicer data look</p>
+</PageContainer>
---
+++ sites/docs/src/routes/guides/feature-guides/column-ordering/_components/datagrid.svelte
@@ -0,0 +1,66 @@
+<script lang="ts">
+	import { setContext } from 'svelte';
+	import { columns } from './columns.svelte';
+	import { inventoryData as data } from '$lib/data/inventory';
+	import { TzezarDatagrid } from '$lib/datagrid/tzezar-datagrid.svelte';
+	import * as Datagrid from '$lib/datagrid';
+
+	let datagrid = setContext(
+		`datagrid`,
+		new TzezarDatagrid({
+			title: 'Column visibility',
+			data,
+			columns,
+			options: {
+				pagination: { display: false },
+				rows: { striped: true },
+				topbar: {
+					display: true,
+					displayCopyDataMenu: false,
+					displayExportDataMenu: false,
+					displayFullscreenToggle: false,
+					displayHeadFilterToggle: false,
+					settingsMenu: {
+						display: true,
+						displayFreezingMenu: false,
+						displayReoderingMenu: true,
+						displayResizingMenu: false,
+						displaySortingMenu: false,
+						displayVisibilityMenu: false,
+						displayMenu: {
+							enabled: false
+						}
+					}
+				}
+			}
+		})
+	);
+</script>
+
+<Datagrid.Datagrid>
+	{#snippet topBar()}
+		<Datagrid.TopBar />
+	{/snippet}
+	{#snippet head()}
+		{#each datagrid.columns as column, i (column.id)}
+			<Datagrid.Header {column}>
+				{#snippet filter()}
+					<Datagrid.ColumnFilter {column} />
+				{/snippet}
+			</Datagrid.Header>
+		{/each}
+	{/snippet}
+	{#snippet body()}
+		{#each datagrid.internal.paginatedData as row, rowIndex}
+			<Datagrid.Row {rowIndex}>
+				{#each datagrid.columns as column, columnIndex}
+					{#if column.id === 'product.name'}
+						<Datagrid.Cell {column} {row} {columnIndex} {rowIndex} class={{ data: 'text-ellipsis text-nowrap overflow-hidden'}}></Datagrid.Cell>
+					{:else}
+						<Datagrid.Cell {column} {row} {columnIndex} {rowIndex}></Datagrid.Cell>
+					{/if}
+				{/each}
+			</Datagrid.Row>
+		{/each}
+	{/snippet}
+</Datagrid.Datagrid>
