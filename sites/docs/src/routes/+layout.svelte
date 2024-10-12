ff921106c5791f9ee34705077180994881c0ead9
Sebastian Drozd <163430081+tzezar@users.noreply.github.com>
2024-10-12 23:17:07 +0200

    fix to that annoying layout shift and logo color change on page load

--- sites/docs/src/routes/+layout.svelte
+++ sites/docs/src/routes/+layout.svelte
@@ -24,6 +24,12 @@
 			}
 		}
 	});
+
+	$effect(()=> {
+		console.log($mode)
+	})
+
+	let schema = $state($mode || 'dark')
 </script>
 
 <svelte:head>
@@ -39,13 +45,13 @@
 			<div class="hidden h-full w-64 shrink-0 flex-col overflow-auto border-r lg:flex">
 				<a href="/" class="">
 					<div
-						class="sticky top-0 flex items-center justify-center gap-1 bg-primary-foreground py-4"
+						class="bg-primary-foreground sticky top-0 flex items-center justify-center gap-1 py-4"
 					>
 						<div class="flex h-10 flex-row items-center justify-center align-middle">
-							{#if $mode == 'dark'}
-								<img src={logoWhite} alt="" srcset="" class="h-[64px]" />
+							{#if schema == 'dark'}
+								<img src={logoWhite} alt="" srcset="" class="h-[64px] w-[64px]" />
 							{:else}
-								<img src={logoBlack} alt="" srcset="" class="h-[64px]" />
+								<img src={logoBlack} alt="" srcset="" class="h-[64px] w-[64px]" />
 							{/if}
 						</div>
 						<div>
@@ -55,7 +61,7 @@
 					</div>
 				</a>
 				<Sidebar />
-				<div class="sticky bottom-0 mt-auto border-t bg-primary-foreground px-8 py-4">
+				<div class="bg-primary-foreground sticky bottom-0 mt-auto border-t px-8 py-4">
 					<div class="flex items-center justify-between gap-4">
 						<a href="https://github.com/tzezar/datagrid" class="font-semibold">GITHUB</a>
 						<ThemeSwitcher />
@@ -63,16 +69,16 @@
 				</div>
 			</div>
 			<div class="flex w-full flex-col overflow-auto">
-				<div class="sticky top-0 z-[19] flex w-full bg-primary-foreground lg:hidden">
+				<div class="bg-primary-foreground sticky top-0 z-[19] flex w-full lg:hidden">
 					<div
-						class="sticky top-0 flex w-full flex-row items-center justify-between gap-1 bg-primary-foreground px-6 py-4"
+						class="bg-primary-foreground sticky top-0 flex w-full flex-row items-center justify-between gap-1 px-6 py-4"
 					>
 						<a href="/" class="flex items-center justify-center gap-1">
 							<div class="flex h-10 flex-row items-center justify-between align-middle">
-								{#if $mode == 'dark'}
-									<img src={logoWhite} alt="" srcset="" class="h-[64px]" />
+								{#if schema == 'dark'}
+									<img src={logoWhite} alt="" srcset="" class="h-[64px] w-[64px]" />
 								{:else}
-									<img src={logoBlack} alt="" srcset="" class="h-[64px]" />
+									<img src={logoBlack} alt="" srcset="" class="h-[64px] w-[64px]" />
 								{/if}
 							</div>
 							<div>
@@ -81,25 +87,27 @@
 							</div>
 						</a>
 
-						<Sheet.Root>
-							<Sheet.Trigger asChild let:builder>
-								<Button builders={[builder]}>Menu</Button>
-							</Sheet.Trigger>
-							<Sheet.Content
-								class="z-[2000] flex h-full  flex-col justify-between overflow-auto p-0"
-							>
-								<Sidebar />
-								<div class="sticky bottom-0 mt-auto border-t bg-primary-foreground px-8 py-4">
-									<div class="flex items-center justify-between gap-4">
-										<a href="https://github.com/tzezar/datagrid" class="font-semibold">GITHUB</a>
-										<ThemeSwitcher />
+						<div>
+							<Sheet.Root>
+								<Sheet.Trigger asChild let:builder>
+									<Button builders={[builder]}>Menu</Button>
+								</Sheet.Trigger>
+								<Sheet.Content
+									class="z-[2000] flex h-full  flex-col justify-between overflow-auto p-0"
+								>
+									<Sidebar />
+									<div class="bg-primary-foreground sticky bottom-0 mt-auto border-t px-8 py-4">
+										<div class="flex items-center justify-between gap-4">
+											<a href="https://github.com/tzezar/datagrid" class="font-semibold">GITHUB</a>
+											<ThemeSwitcher />
+										</div>
 									</div>
-								</div>
-							</Sheet.Content>
-						</Sheet.Root>
+								</Sheet.Content>
+							</Sheet.Root>
+						</div>
 					</div>
 				</div>
-				<div class="p-8 grow">
+				<div class="grow p-8">
 					{@render children()}
 				</div>
 			</div>
