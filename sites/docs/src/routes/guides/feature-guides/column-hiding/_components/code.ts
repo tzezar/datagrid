06682df9beeb35b82df7387fed0a915ce98a969f
Sebastian Drozd <163430081+tzezar@users.noreply.github.com>
2024-10-12 18:46:49 +0200

    add column hiding doc

---
+++ sites/docs/src/routes/guides/feature-guides/column-hiding/_components/code.ts
@@ -0,0 +1,77 @@
+export const code = {
+    baseColumns: `export type BaseColumn<T = unknown> = {
+    visible?: boolean;
+    hideable?: boolean;
+};`,
+    simpleColumn: `{
+    id: 'barcode',
+    title: 'Barcode',
+    pinned: {
+        position: 'left'
+    }
+}`,
+    columns: `import type { BaseColumn } from "$lib/datagrid/types";
+import type { InventoryDataRow } from "$lib/data/inventory";
+
+export const columns = [
+    {
+        id: 'barcode',
+        title: 'Barcode',
+        visible: false,
+        grow: true,
+    },
+
+    {
+        id: 'location',
+        title: 'Location',
+        hideable: false,
+        grow: true,
+    },
+
+] satisfies BaseColumn<InventoryDataRow>[]
+`,
+
+    important1: `satisfies BaseColumn<InventoryDataRow>[]`,
+    datagrid: `<script lang="ts">
+	import { setContext } from 'svelte';
+	import { columns } from './columns.svelte';
+	import { inventoryData as data } from '$lib/data/inventory';
+	import { TzezarDatagrid } from '$lib/datagrid/tzezar-datagrid.svelte';
+	import * as Datagrid from '$lib/datagrid';
+
+	let datagrid = setContext(
+		\`datagrid\`,
+		new TzezarDatagrid({
+			title: 'Column freezing',
+			data,
+			columns,
+			options: {...}
+		})
+	);
+</script>
+
+<Datagrid.Datagrid>
+	{#snippet topBar()}
+		<Datagrid.TopBar />
+	{/snippet}
+	{#snippet head()}
+		{#each datagrid.columns as column, i (column.id)}
+			<Datagrid.Header {column}>
+				{#snippet filter()}
+					<Datagrid.ColumnFilter {column} />
+				{/snippet}
+			</Datagrid.Header>
+		{/each}
+	{/snippet}
+	{#snippet body()}
+		{#each datagrid.internal.paginatedData as row, rowIndex}
+			<Datagrid.Row {rowIndex}>
+				{#each datagrid.columns as column, columnIndex}
+					<Datagrid.Cell {column} {row} {columnIndex} {rowIndex}></Datagrid.Cell>
+				{/each}
+			</Datagrid.Row>
+		{/each}
+	{/snippet}
+</Datagrid.Datagrid>
+`
+}
