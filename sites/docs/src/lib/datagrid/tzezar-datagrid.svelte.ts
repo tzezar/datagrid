da89f06d0b407289651c0954df509ee1141ba938
Sebastian Drozd <163430081+tzezar@users.noreply.github.com>
2024-10-13 00:52:50 +0200

    major improvement

--- sites/docs/src/lib/datagrid/tzezar-datagrid.svelte.ts
+++ sites/docs/src/lib/datagrid/tzezar-datagrid.svelte.ts
@@ -2,34 +2,28 @@ import { applyOffset } from "./fns/apply-offset";
 import { filterData } from "./fns/filter-data";
 import { paginateData } from "./fns/paginate-data";
 import { sortData } from "./fns/sort-data";
-import type { BaseColumn, ExpandedRows, Filter, SelectionPoint, SelectionRange, SelectionState, Sorting, Pagination } from "./types";
+import type { BaseColumn, ExpandedRows, Filter, SelectionState, Sorting } from "./types";
 import { SvelteSet } from "svelte/reactivity";
 
 export class TzezarDatagrid<T, C extends BaseColumn<T> = BaseColumn<T>> {
-    mode: 'client' | 'server' = $state('client');
-
-    // fast workaround for svelte-query bugs
-    // ? maybe it is a good idea to add pre-made listeners for other events too?
-    onPageChange = () => { };
-    onPerPageChange = () => { };
-    onSortingChange = () => { };
-    onFiltersChange = () => { };
-    onChange = () => { };
+    // Public properties
+    mode = $state('client');
+    columns = $state<C[]>([]);
+    data = $state<T[]>([]);
+    title = $state('');
     identifier = $state('1');
-    columns: C[] = $state([]);
-    data: T[] = $state([]);
-    title: string = $state('')
+
+    // Lifecycle hooks
+    onPageChange = () => {};
+    onPerPageChange = () => {};
+    onSortingChange = () => {};
+    onFiltersChange = () => {};
+    onChange = () => {};
+
+    // State management
     state = $state({
-        pagination: {
-            page: 1,
-            perPage: 20,
-            count: 1
-        } as Pagination,
-        status: {
-            isFetching: false,
-            isError: false,
-            isRefetching: false
-        },
+        pagination: { page: 1, perPage: 20, count: 1 },
+        status: { isFetching: false, isError: false, isRefetching: false },
         sortingArray: [] as Sorting[],
         filters: [] as Filter[],
         expandedRows: [] as ExpandedRows,
@@ -37,33 +31,31 @@ export class TzezarDatagrid<T, C extends BaseColumn<T> = BaseColumn<T>> {
         isFullscreenActive: false,
         isHeadFilterVisible: false
     });
+
+    // Internal state
     internal = $state({
         paginatedData: [] as T[],
         sortedData: [] as T[],
         filteredData: [] as T[],
         selectionState: {
-            start: null as SelectionPoint | null,
-            end: null as SelectionPoint | null,
-            range: new SvelteSet() as SelectionRange,
-            activeRange: new SvelteSet() as SelectionRange,
+            start: null,
+            end: null,
+            range: new SvelteSet(),
+            activeRange: new SvelteSet(),
             isSelecting: false,
             isRemoving: false,
             isMouseDown: false
         } as SelectionState,
-        keyboardNavigation: {
-            focusedRowIndex: 0,
-            focusedColumnIndex: 0
-        },
-        // ? this is optimization to make other functions like pagination more performant
-        // ? it stores just ids as set instead of the whole row object
-        selectedRowIds: new SvelteSet() as SvelteSet<number>,
+        keyboardNavigation: { focusedRowIndex: 0, focusedColumnIndex: 0 },
+        selectedRowIds: new SvelteSet<number>(),
         headSize: -1,
     });
 
+    // Options
     options = $state({
         scrollable: true,
         fullscreenMode: { enabled: true },
-        pagination: { display: false, },
+        pagination: { display: false },
         dataIndicator: { display: true },
         statusIndicator: { display: true },
         rows: { striped: false },
@@ -84,7 +76,6 @@ export class TzezarDatagrid<T, C extends BaseColumn<T> = BaseColumn<T>> {
                     enabled: false,
                     displaySpacingMenu: true,
                     displayTextSizeMenu: true
-
                 }
             }
         },
@@ -98,203 +89,81 @@ export class TzezarDatagrid<T, C extends BaseColumn<T> = BaseColumn<T>> {
                 lg: { vertical: '20px', horizontal: '20px' },
                 xl: { vertical: '30px', horizontal: '30px' },
             },
-            selected: {
-                label: 'md',
-                vertical: '10px',
-                horizontal: '10px',
-            }
+            selected: { label: 'md', vertical: '10px', horizontal: '10px' }
         },
         fontSize: {
             options: {
-                xs: '0.75rem',
-                sm: '0.875rem',
-                md: '1rem',
-                lg: '1.25rem',
-                xl: '1.5rem',
+                xs: '0.75rem', sm: '0.875rem', md: '1rem', lg: '1.25rem', xl: '1.5rem',
             },
-            selected: {
-                label: 'md',
-                value: '1rem',
-            }
+            selected: { label: 'md', value: '1rem' }
         },
     });
 
-    // TODO: Rewrite this to be clener
-    // * object assign does not work for some reason, dont have time rn to figure it out
-    constructor({
-        mode,
-        columns,
-        data,
-        identifier,
-        title,
-        options,
-        state,
-        internal,
-        onPageChange,
-        onPerPageChange,
-        onSortingChange,
-        onFiltersChange,
-        onChange
-    }: ConstructorOptions & {
-        columns: C[],
-        data: T[],
-    }) {
-        this.onPageChange = onPageChange || this.onPageChange
-        this.onPerPageChange = onPerPageChange || this.onPerPageChange
-        this.onSortingChange = onSortingChange || this.onSortingChange
-        this.onFiltersChange = onFiltersChange || this.onFiltersChange
-        this.onChange = onChange || this.onChange
+    constructor(config: TzezarDatagridConfig<T, C>) {
+        this.initializeFromConfig(config);
+        this.initializeData();
+    }
 
-        this.columns = applyOffset(columns) as C[]
+    // Initialize the datagrid with the provided configuration
+    private initializeFromConfig(config: TzezarDatagridConfig<T, C>) {
+        const { mode, columns, data, identifier, title, options, state, onPageChange, onPerPageChange, onSortingChange, onFiltersChange, onChange } = config;
+
+        // Set core properties
+        this.mode = mode || this.mode;
+        // @ts-expect-error ts(2322) 
+        this.columns = applyOffset(columns);
         this.data = data;
-
-        this.state.filters = state?.filters || this.state.filters
-
         this.identifier = identifier || this.identifier;
+        this.title = title || this.title;
 
-        // options
-        this.options.scrollable = options?.scrollable ?? this.options.scrollable
-        this.options.fullscreenMode = options?.fullscreenMode ?? this.options.fullscreenMode
-        this.options.pagination = options?.pagination ?? this.options.pagination
-        this.options.statusIndicator = options?.statusIndicator ?? this.options.statusIndicator
-        this.options.dataIndicator = options?.dataIndicator ?? this.options.dataIndicator
-        this.options.footer = options?.footer ?? this.options.footer
-        this.options.topbar.display = options?.topbar?.display ?? this.options.topbar.display
-        this.title = title ?? this.title
-        this.options.rows.striped = options?.rows?.striped ?? this.options.rows.striped
+        // Set event handlers
+        this.onPageChange = onPageChange || this.onPageChange;
+        this.onPerPageChange = onPerPageChange || this.onPerPageChange;
+        this.onSortingChange = onSortingChange || this.onSortingChange;
+        this.onFiltersChange = onFiltersChange || this.onFiltersChange;
+        this.onChange = onChange || this.onChange;
 
-        this.options.topbar.displayFullscreenToggle = options?.topbar?.displayFullscreenToggle ?? this.options.topbar.displayFullscreenToggle
-        this.options.topbar.displayExportDataMenu = options?.topbar?.displayExportDataMenu ?? this.options.topbar.displayExportDataMenu
-        this.options.topbar.displayCopyDataMenu = options?.topbar?.displayCopyDataMenu ?? this.options.topbar.displayCopyDataMenu
+        // Initialize state
+        this.initializeState(state);
 
-        this.options.topbar.displayHeadFilterToggle = options?.topbar?.displayHeadFilterToggle ?? this.options.topbar.displayHeadFilterToggle
+        // Initialize options
+        this.initializeOptions(options);
+    }
 
-        this.options.topbar.settingsMenu.display = options?.topbar?.settingsMenu?.display ?? this.options.topbar.settingsMenu.display
+    // Initialize state with provided values or defaults
+    private initializeState(state?: Partial<typeof this.state>) {
+        if (state) {
+            Object.assign(this.state, state);
+        }
+    }
 
-        this.options.topbar.settingsMenu.displaySortingMenu = options?.topbar?.settingsMenu?.displaySortingMenu ?? this.options.topbar.settingsMenu.displaySortingMenu
-        this.options.topbar.settingsMenu.displayReoderingMenu = options?.topbar?.settingsMenu?.displayReoderingMenu ?? this.options.topbar.settingsMenu.displayReoderingMenu
+    // Initialize options with provided values or defaults
+    private initializeOptions(options?: Partial<typeof this.options>) {
+        if (options) {
+            Object.assign(this.options, options);
+        }
+    }
 
-        this.options.topbar.settingsMenu.displayFreezingMenu = options?.topbar?.settingsMenu?.displayFreezingMenu ?? this.options.topbar.settingsMenu.displayFreezingMenu
-        this.options.topbar.settingsMenu.displayVisibilityMenu = options?.topbar?.settingsMenu?.displayVisibilityMenu ?? this.options.topbar.settingsMenu.displayVisibilityMenu
-
-        this.options.topbar.settingsMenu.displayResizingMenu = options?.topbar?.settingsMenu?.displayResizingMenu ?? this.options.topbar.settingsMenu.displayResizingMenu
-        this.options.topbar.settingsMenu.displayMenu.enabled = options?.topbar?.settingsMenu?.displayMenu?.enabled ?? this.options.topbar.settingsMenu.displayMenu.enabled
-        this.options.topbar.settingsMenu.displayMenu.displaySpacingMenu = options?.topbar?.settingsMenu?.displayMenu?.displaySpacingMenu ?? this.options.topbar.settingsMenu.displayMenu.displaySpacingMenu
-        this.options.topbar.settingsMenu.displayMenu.displayTextSizeMenu = options?.topbar?.settingsMenu?.displayMenu?.displayTextSizeMenu ?? this.options.topbar.settingsMenu.displayMenu.displayTextSizeMenu
-
-        this.mode = mode || this.mode
-
-        this.state.pagination.count = state?.pagination?.count || this.state.pagination.count
-        this.state.pagination.page = state?.pagination?.page || this.state.pagination.page
-        this.state.pagination.perPage = state?.pagination?.perPage || this.state.pagination.perPage
-
-        this.state.status.isFetching = state?.status?.isFetching || this.state.status.isFetching
-        this.state.status.isError = state?.status?.isError || this.state.status.isError
-        this.state.status.isRefetching = state?.status?.isRefetching || this.state.status.isRefetching
-
-        this.state.sortingArray = state?.sortingArray || this.state.sortingArray
-
-        this.state.expandedRows = state?.expandedRows || this.state.expandedRows
-
-        this.state.selectedRows = state?.selectedRows || this.state.selectedRows
-
-        this.state.isFullscreenActive = state?.isFullscreenActive || this.state.isFullscreenActive
-
-        this.state.isHeadFilterVisible = state?.isHeadFilterVisible || this.state.isHeadFilterVisible
-
-
-        this.internal.paginatedData = internal?.paginatedData || this.internal.paginatedData
-
-        this.internal.sortedData = internal?.sortedData || this.internal.sortedData
-
-        this.internal.filteredData = internal?.filteredData || this.internal.filteredData
-
-        // provide defaults for SSR
-        this.internal.sortedData = sortData([...data], this.state.sortingArray)
-        this.internal.filteredData = filterData([...this.internal.sortedData], this.state.filters)
-        this.internal.paginatedData = paginateData([...this.internal.filteredData], this.state.pagination.page, this.state.pagination.perPage)
-
-
+    // Initialize data by sorting, filtering, and paginating
+    private initializeData() {
+        this.internal.sortedData = sortData([...this.data], this.state.sortingArray);
+        this.internal.filteredData = filterData([...this.internal.sortedData], this.state.filters);
+        this.internal.paginatedData = paginateData([...this.internal.filteredData], this.state.pagination.page, this.state.pagination.perPage);
     }
 }
 
-
-export type ConstructorOptions = {
-    mode?: 'client' | 'server',
-    onPageChange?: () => void,
-    onPerPageChange?: () => void,
-    onSortingChange?: () => void,
-    onFiltersChange?: () => void,
-    onChange?: () => void,
-    identifier?: string,
-    title?: string,
-
-    state?: {
-        pagination?: {
-            page?: number,
-            perPage?: number,
-            count?: number
-        },
-        status?: {
-            isFetching?: boolean,
-            isError?: boolean,
-            isRefetching?: boolean
-        },
-        sortingArray?: [],
-        filters?: [],
-        expandedRows?: [],
-        selectedRows?: [],
-        isFullscreenActive?: boolean,
-        isHeadFilterVisible?: boolean
-    },
-    internal?: {
-        paginatedData?: [],
-        sortedData?: [],
-        filteredData?: [],
-    },
-
-
-
-    options?: {
-        scrollable?: boolean,
-        fullscreenMode?: {
-            enabled: boolean
-        },
-        pagination?: {
-            display: boolean
-        },
-        statusIndicator?: {
-            display: boolean
-        },
-        dataIndicator?: {
-            display: boolean
-        },
-        footer?: {
-            display: boolean
-        }
-        topbar?: {
-            display: boolean,
-            displayFullscreenToggle?: boolean,
-            displayExportDataMenu?: boolean,
-            displayCopyDataMenu?: boolean,
-            displayHeadFilterToggle?: boolean,
-            settingsMenu?: {
-                display: boolean,
-                displaySortingMenu?: boolean,
-                displayReoderingMenu?: boolean,
-                displayFreezingMenu?: boolean,
-                displayResizingMenu?: boolean,
-                displayVisibilityMenu?: boolean,
-                displayMenu?: {
-                    enabled: boolean,
-                    displaySpacingMenu?: boolean,
-                    displayTextSizeMenu?: boolean
-                }
-            }
-        }
-        rows?: {
-            striped: boolean
-        },
-    }
-}
-
+// Configuration type for the TzezarDatagrid constructor
+type TzezarDatagridConfig<T, C extends BaseColumn<T>> = {
+    mode?: 'client' | 'server';
+    columns: C[];
+    data: T[];
+    identifier?: string;
+    title?: string;
+    options?: Partial<TzezarDatagrid<T, C>['options']>;
+    state?: Partial<TzezarDatagrid<T, C>['state']>;
+    onPageChange?: () => void;
+    onPerPageChange?: () => void;
+    onSortingChange?: () => void;
+    onFiltersChange?: () => void;
+    onChange?: () => void;
+};
