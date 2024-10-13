f29dd91182b1fab9f4f0bfdf72871aba6497918b
Sebastian Drozd <163430081+tzezar@users.noreply.github.com>
2024-10-13 02:16:14 +0200

    Update is-every-row-selected.ts

--- sites/docs/src/lib/datagrid/fns/is-every-row-selected.ts
+++ sites/docs/src/lib/datagrid/fns/is-every-row-selected.ts
@@ -1,3 +1,10 @@
-export const isEveryRowSelected = (selectedRows: unknown[], data: unknown[]) => {
+/**
+ * Checks if every row in the data set is selected.
+ *
+ * @param selectedRows - An array of selected rows.
+ * @param data - An array of all data rows.
+ * @returns True if every row is selected, false otherwise.
+ */
+export const isEveryRowSelected = <T>(selectedRows: T[], data: T[]): boolean => {
     return selectedRows.length === data.length;
 };
