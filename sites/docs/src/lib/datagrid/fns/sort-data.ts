b495cc929edbd8b39a9612756a8fb002064ebb93
Sebastian Drozd <163430081+tzezar@users.noreply.github.com>
2024-10-13 02:23:41 +0200

    Update sort-data.ts

--- sites/docs/src/lib/datagrid/fns/sort-data.ts
+++ sites/docs/src/lib/datagrid/fns/sort-data.ts
@@ -1,94 +1,42 @@
-// import type { Sorting } from "../types";
-
-// function createCompositeKey<T>(item: T, sorting: Sorting[]): string {
-//     return sorting.map(({ columnId, direction }) => {
-//         const value = item[columnId];
-//         return `${direction === 'asc' ? '' : '-'}${value}`;
-//     }).join('|');
-// }
-
-// export function sortData<T>(data: T[], sorting: Sorting[]): T[] {
-//     console.log('sort')
-//     return data.sort((a, b) => {
-//         const keyA = createCompositeKey(a, sorting);
-//         const keyB = createCompositeKey(b, sorting);
-//         return keyA.localeCompare(keyB);
-//     });
-// }
-
-// import type { Sorting } from "../types";
-
-
-// // it looks like thise one is more performant then composite key solution
-// export const sortData = <T>(data: T[], sorting: Sorting[]): T[] => {
-//     console.log('sorted')
-//     return data.sort((a, b) => {
-//         for (const sort of sorting) {
-//             const { columnId, direction } = sort;
-//             const aValue = a[columnId];
-//             const bValue = b[columnId];
-
-//             let comparison = 0;
-//             if (aValue < bValue) {
-//                 comparison = -1;
-//             } else if (aValue > bValue) {
-//                 comparison = 1;
-//             }
-
-//             // Reverse the comparison for descending order
-//             if (direction === 'desc') {
-//                 comparison = comparison * -1;
-//             }
-
-//             // If comparison is not zero, return the result
-//             if (comparison !== 0) {
-//                 return comparison;
-//             }
-//         }
-//         return 0; // If all sorting criteria are equal
-//     });
-// }
-
-
-// export function sortData<T>(data: T[], sorting: Sorting[]): T[] {
-//     return sort(data).by(sorting.map(({ columnId, direction }) => {
-//         return direction === 'asc' ? (item: T) => item[columnId] : (item: T) => -item[columnId];
-//     }));
-// }
-
-
-
-
 import { sort, createNewSortInstance } from 'fast-sort';
 import type { Sorting } from '../types';
 import { getNestedValue } from './get-nested-value';
 
+// Create a natural sorting instance using Intl.Collator for better string comparison
 const naturalSort = createNewSortInstance({
     comparer: new Intl.Collator(undefined, { numeric: true, sensitivity: 'base' }).compare,
 });
 
-// * this one is the fastest!
-// TODO: type it better
+// Function to sort data based on specified sorting criteria
 export function sortData<T>(data: T[], sorting: Sorting[]): T[] {
+    // If no sorting criteria are provided, return the original data
     if (sorting.length === 0) {
         return data;
-    } else if (sorting.length === 1) {
-        const { columnId, direction } = sorting[0];
-        if (direction === 'asc') {
-            return sort(data).asc(item => getNestedValue(item, columnId));
-        } else {
-            return sort(data).desc(item => getNestedValue(item, columnId));
-        }
-    } else {
-        const sortingArray: { [key: string]: (item: T) => unknown }[] = [];
-        for (const sortItem of sorting) {
-            const { columnId, direction } = sortItem;
-            sortingArray.push({
-                [direction]: (item: T) => getNestedValue(item, columnId)
-            });
-        }
-        // TODO: type it properly
-        // @ts-ignore
-        return sort(data).by(sortingArray);
     }
+
+    // If only one sorting criterion is specified, sort accordingly
+    if (sorting.length === 1) {
+        const { columnId, direction } = sorting[0];
+        // Use ascending or descending sorting based on the direction
+        return direction === 'asc' 
+            ? sort(data).asc(item => getNestedValue(item, columnId))
+            : sort(data).desc(item => getNestedValue(item, columnId));
+    } 
+    
+    // Prepare an array for multiple sorting criteria
+    const sortingArray: Array<{ [key: string]: (item: T) => unknown }> = [];
+
+    // Build sorting criteria based on the provided sorting input
+    for (const sortItem of sorting) {
+        const { columnId, direction } = sortItem;
+
+        // Push each sort criteria into the sortingArray
+        sortingArray.push({
+            [direction]: (item: T) => getNestedValue(item, columnId)
+        });
+    }
+
+    // Sort the data using the compiled sorting criteria
+    // Here we cast the sortingArray to 'any' to satisfy TypeScript
+    return sort(data).by(sortingArray as any);
 }
