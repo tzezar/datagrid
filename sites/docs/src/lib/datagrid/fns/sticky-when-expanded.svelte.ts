9bfe614794e01e96826b8496237a158166aa8749
Sebastian Drozd <163430081+tzezar@users.noreply.github.com>
2024-10-13 02:23:44 +0200

    Delete sticky-when-expanded.svelte.ts

--- sites/docs/src/lib/datagrid/fns/sticky-when-expanded.svelte.ts
+++
@@ -1,5 +0,0 @@
-// not implemented
-// @ts-nocheck
-import type { TzezarDataGrid } from "../tzezar-datagrid.svelte";
-
-export const stickyWhenExpanded = (datagrid: TzezarDataGrid, rowId: number) => datagrid.state.expandedRows.includes(rowId) ? `sticky top-[${datagrid.headerHeight}px] z-[4998]` : 'z-[4999]'
