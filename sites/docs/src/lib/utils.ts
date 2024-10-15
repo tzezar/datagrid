60cbce340b5c79884941865a45b9a861a26f857e
Sebastian Drozd <163430081+tzezar@users.noreply.github.com>
2024-10-15 03:30:18 +0200

    Update utils.ts

--- sites/docs/src/lib/utils.ts
+++ sites/docs/src/lib/utils.ts
@@ -60,3 +60,11 @@ export const flyAndScale = (
 		easing: cubicOut
 	};
 };
+
+
+type DateStyle = Intl.DateTimeFormatOptions['dateStyle']
+
+export function formatDate(date: string, dateStyle: DateStyle = 'medium', locales = 'en') {
+	const formatter = new Intl.DateTimeFormat(locales, { dateStyle })
+	return formatter.format(new Date(date))
+}
