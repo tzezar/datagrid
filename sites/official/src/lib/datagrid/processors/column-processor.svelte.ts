de7a1b9c5a0d708a90afb01095b956b2c7d5effd
Sebastian Drozd <tzezar44@gmail.com>
2024-11-03 23:13:06 +0100

    fixed #32 #23

--- sites/official/src/lib/datagrid/processors/column-processor.svelte.ts
+++ sites/official/src/lib/datagrid/processors/column-processor.svelte.ts
@@ -1,4 +1,5 @@
 import type { ColumnDef } from "../../../v2/_components/types";
+import type { SortDirection } from "../features/sorting-manager.svelte";
 import type { DatagridInstance } from "../index.svelte";
 
 
@@ -9,7 +10,12 @@ export interface Column {
         width: number
         minWidth: number
         maxWidth: number
-    }
+    },
+
+
+    isSorted: () => boolean
+    getSortingDirection: () => SortDirection
+
 }
 
 export interface ColumnProcessorInstance {
@@ -29,7 +35,13 @@ export class ColumnProcessor implements ColumnProcessorInstance {
         for (let i = 0; i < this.grid.original.columns.length; i++) {
             const { accessorKey, header } = this.grid.original.columns[i];
 
+
+            const isSorted = () => this.grid.sorting.sortBy.filter((s) => s.accessor === accessorKey).length > 0;
+            const getSortingDirection = () => this.grid.sorting.sortBy.filter((s) => s.accessor === accessorKey)[0]?.direction
+            
             columns.push({
+                isSorted,
+                getSortingDirection,
                 accessorKey,
                 header,
                 size: {
