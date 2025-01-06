c4ca24d3aaaaa825bbbf14a06250e7850b3ae82e
Sebastian Drozd <tzezar44@gmail.com>
2025-01-06 01:41:59 +0100

    added nice animations

--- sites/official/src/routes/shadcn-svelte/default/+page.svelte
+++ sites/official/src/routes/shadcn-svelte/default/+page.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import { flip } from 'svelte/animate';
 	import '$lib/datagrid/styles.css';
 	import { Datagrid } from '$lib/datagrid/core/index.svelte';
 	import { userColumns } from './columns.svelte';
@@ -8,6 +9,8 @@
 	import GroupBy from '$lib/datagrid/prebuilt/shadcn-svelte/controls/group-by.svelte';
 	import HeaderCell from '$lib/datagrid/prebuilt/shadcn-svelte/header-cell.svelte';
 	import Row from '$lib/datagrid/prebuilt/shadcn-svelte/row.svelte';
+	import { cubicInOut } from 'svelte/easing';
+	import { fade, fly } from 'svelte/transition';
 
 	let { data } = $props();
 
@@ -33,24 +36,42 @@
 			</div>
 			<div class="grid-body">
 				{#each datagrid.rowPinning.getTopRows() as row (row.index)}
-				<Row {datagrid} {row} />
-			{/each}
+					<div
+						in:fade={{ duration: 100, delay: 100 }}
+						animate:flip={{ easing: cubicInOut, duration: 200 }}
+					>
+						<Row {datagrid} {row} />
+					</div>
+				{/each}
 
-			{#each datagrid.rowPinning.getCenterRows() as row (row.index)}
-				<Row {datagrid} {row} />
-			{/each}
+				{#each datagrid.rowPinning.getCenterRows() as row (row.identifier)}
+					<div
+						in:fade={{ duration: 100, delay: 100 }}
+						animate:flip={{ easing: cubicInOut, duration: 200 }}
+					>
+						<Row {datagrid} {row} />
+					</div>
+				{/each}
 
-			{#each datagrid.rowPinning.getBottomRows() as row (row.index)}
-				<Row {datagrid} {row} />
-			{/each}
-				<!-- {#each datagrid.cache.paginatedRows || [] as row (row.index)}
-					<Row {datagrid} {row} />
-				{/each} -->
+				{#each datagrid.rowPinning.getBottomRows() as row (row.index)}
+					<div
+						in:fade={{ duration: 100, delay: 100 }}
+						animate:flip={{ easing: cubicInOut, duration: 200 }}
+					>
+						<Row {datagrid} {row} />
+					</div>
+				{/each}
 			</div>
 		</div>
-		<div class='grid-footer-container'>
+		<div class="grid-footer-container">
 			<Pagination {datagrid} />
 			<GroupBy {datagrid} />
 		</div>
 	</div>
 </div>
+
+<style>
+	.grid-body > div {
+		will-change: transform;
+	}
+</style>
--- sites/official/src/routes/shadcn-svelte/default/+page.ts
+++ sites/official/src/routes/shadcn-svelte/default/+page.ts
@@ -1,6 +1,6 @@
 import { generateUsers } from "./generate-users";
 
 export const load = async () => {
-    const users = generateUsers(1000);
+    const users = generateUsers(100000);
     return { users };
   };
