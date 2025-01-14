7737f003ef5da549cfb0017708ed5f22d8f9c73a
Sebastian Drozd <tzezar44@gmail.com>
2025-01-14 21:45:55 +0100

    template

---
+++ sites/official/src/routes/headless/examples/[slug]/+page.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+	import { formatDate } from '$lib/utils';
+
+	export let data;
+</script>
+
+<!-- SEO -->
+<svelte:head>
+	<title>{data.meta.title}</title>
+	<meta property="og:type" content="article" />
+	<meta property="og:title" content={data.meta.title} />
+</svelte:head>
+
+<article class="flex w-full flex-col">
+	<group>
+		<h1 class="text-3xl font-bold">{data.meta.title}</h1>
+		<p class="text-muted-foreground text-xs">Published at {formatDate(data.meta.date)}</p>
+	</group>
+	<div class="prose dark:prose-invert pt-8 min-w-full ">
+		<svelte:component this={data.content} />
+	</div>
+</article>
+
+<style>
+</style>
