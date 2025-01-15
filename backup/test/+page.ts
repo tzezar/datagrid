170293a1f7afd5d810c35756f62474b608306bc8
Sebastian Drozd <tzezar44@gmail.com>
2025-01-15 18:53:34 +0100

    cleanup

--- backup/test/+page.ts
+++
@@ -1,6 +0,0 @@
-import { generateUsers } from "./generate-users";
-
-export const load = async () => {
-    const users = generateUsers(100000);
-    return { users };
-  };
--- old/playground/_components/cells/profit.svelte
+++
@@ -1,9 +0,0 @@
-<script lang='ts'>
-	import type { Row } from "$lib/tzezars-datagrid/processors/data-processor.svelte";
-
-
-    let {row}: {row: Row} = $props();
-
-</script>
-
-{row?.original?.profit} custom cell
