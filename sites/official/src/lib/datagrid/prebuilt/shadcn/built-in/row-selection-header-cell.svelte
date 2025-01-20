936a2e772951f0716f53acf8621379629ad59a42
Sebastian Drozd <tzezar44@gmail.com>
2025-01-20 20:12:35 +0100

    Update row-selection-header-cell.svelte

--- sites/official/src/lib/datagrid/prebuilt/shadcn/built-in/row-selection-header-cell.svelte
+++ sites/official/src/lib/datagrid/prebuilt/shadcn/built-in/row-selection-header-cell.svelte
@@ -32,9 +32,8 @@
 <DropdownMenu.Root>
 	<DropdownMenu.Trigger class='flex items-start'>
 		<button class="bg-primary size-4 m-2" aria-label="Toggle row selection"></button>
-		<!-- <Button size="sm" class="m-0 h-full rounded-none">O</Button> -->
 	</DropdownMenu.Trigger>
-	<DropdownMenu.Content>
+	<DropdownMenu.Content side="right">
 		<DropdownMenu.Group>
 			<DropdownMenu.Label>Row selection</DropdownMenu.Label>
 			<DropdownMenu.Separator />
