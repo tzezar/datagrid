ee165b35e76ab48718b1941e7d9de27f0cdfb6c6
Sebastian Drozd <tzezar44@gmail.com>
2025-01-27 03:07:19 +0100

    fixes

--- sites/official/src/routes/shadcn-svelte/examples/row-selection/+page.ts
+++ sites/official/src/routes/shadcn-svelte/examples/row-selection/+page.ts
@@ -1,9 +1,10 @@
 import { generateData } from "$lib/data-generators/generate-data";
 import { generateInventoryItem } from "$lib/data-generators/generate/inventory";
+import { generateUser } from "$lib/data-generators/generate/user";
 
 export const ssr = false;
 
 export const load = async () => {
-    const users = generateData(generateInventoryItem, 100000);
+    const users = generateData(generateUser, 100000);
     return { users };
   };
