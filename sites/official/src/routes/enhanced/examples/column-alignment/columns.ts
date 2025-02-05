440d815d829a7c77e4f495715d59a0f0e23e43d1
Sebastian Drozd <tzezar44@gmail.com>
2025-02-05 21:43:17 +0100

    feat:

--- sites/official/src/routes/enhanced/examples/column-alignment/columns.ts
+++ sites/official/src/routes/enhanced/examples/column-alignment/columns.ts
@@ -5,8 +5,16 @@ import type { InventoryItem } from "$lib/data-generators/generate/inventory";
 import type { ColumnMetaEnhanced } from "$lib/datagrid-enhanced/core/types";
 
 export const columns = [
-    accessorColumn({ accessorKey: 'id', align: "right" }),
-    accessorColumn({ accessorKey: 'name', align: 'left' }),
-    accessorColumn({ accessorKey: 'category', align: 'center' }),
-    accessorColumn({ accessorKey: 'price', _meta: { grow: true }, align: 'right' }),
+    accessorColumn({
+        accessorKey: 'id', align: "right"
+    }),
+    accessorColumn({
+        accessorKey: 'name', align: 'left'
+    }),
+    accessorColumn({
+        accessorKey: 'category', align: 'center'
+    }),
+    accessorColumn({
+        accessorKey: 'price', _meta: { grow: true }, align: 'right'
+    }),
 ] satisfies AnyColumn<InventoryItem, ColumnMetaEnhanced<InventoryItem>>[]
---
+++ sites/official/src/routes/enhanced/examples/column-filters/datagrid.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import type { InventoryItem } from '$lib/data-generators/generate/inventory';
+	import * as Grid from '$lib/datagrid-enhanced';
+	import { columns } from './columns';
+
+	let { data }: { data: InventoryItem[] } = $props();
+
+	let datagrid = new Grid.EnhancedCore({
+		columns,
+		data
+	});
+
+</script>
+
+<Grid.Component {datagrid} />
