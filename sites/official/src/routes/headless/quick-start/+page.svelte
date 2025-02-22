b8ba66e81e9827ac14ec57a6af78e1f85ac0c85f
Sebastian Drozd <163430081+tzezar@users.noreply.github.com>
2025-02-22 17:56:51 +0100

    feat: breadcrumbs support added

---
+++ sites/official/src/routes/headless/quick-start/+page.svelte
@@ -0,0 +1,14 @@
+<script lang='ts'>
+	import Link from "$lib/components/tzezars-enhancements/link/link.svelte";
+import { navigationTree } from "../../_layout/navigation-tree";
+
+    let tree = navigationTree.navQuickStart
+</script>
+
+<div class='flex flex-col gap-4'>
+    {#each tree as item}
+    <Link href={item.url}>{item.title}</Link>
+{/each}
+
+</div>
+  
