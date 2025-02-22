46230832900acaa69bd63955526dc19d97f2d82e
Sebastian Drozd <163430081+tzezar@users.noreply.github.com>
2025-02-22 02:17:25 +0100

    feat:

---
+++ sites/official/src/routes/_layout/nav-quick-start.svelte
@@ -0,0 +1,87 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+	import Badge from '$lib/components/ui/badge/badge.svelte';
+	import * as Collapsible from '$lib/components/ui/collapsible/index.js';
+	import * as Sidebar from '$lib/components/ui/sidebar/index.js';
+	import ChevronRight from 'lucide-svelte/icons/chevron-right';
+
+	let {
+		items
+	}: {
+		items: {
+			title: string;
+			url: string;
+			// this should be `Component` after lucide-svelte updates types
+			// eslint-disable-next-line @typescript-eslint/no-explicit-any
+			icon?: any;
+			isActive?: boolean;
+			items?: {
+				title: string;
+				url: string;
+			}[];
+		}[];
+	} = $props();
+</script>
+
+<Sidebar.Group class='p-0'>
+	<Sidebar.GroupLabel>Quick Start</Sidebar.GroupLabel>
+	<Sidebar.Menu>
+		{#each items as mainItem (mainItem.title)}
+			<Collapsible.Root open={mainItem.isActive} class="group/collapsible">
+				{#snippet child({ props })}
+					<Sidebar.MenuItem {...props}>
+						<Collapsible.Trigger>
+							{#snippet child({ props })}
+								{#if mainItem.items}
+									<Sidebar.MenuButton {...props}>
+										{#if mainItem.icon}
+											<mainItem.icon />
+										{/if}
+										<span>{mainItem.title}</span>
+										<ChevronRight
+											class="ml-auto transition-transform duration-200 group-data-[state=open]/collapsible:rotate-90"
+										/>
+									</Sidebar.MenuButton>
+								{/if}
+								{#if !mainItem.items}
+									<a href={mainItem.url} {...props}>
+										<Sidebar.MenuButton
+											{...props}
+											isActive={mainItem.url === $page.url.pathname}
+										>
+											{#if mainItem.icon}
+												<mainItem.icon />
+											{/if}
+											<span>{mainItem.title}</span>
+											{#if mainItem?.badge}
+												<Badge class="ml-auto rounded-sm" variant="outline">{mainItem?.badge}</Badge
+												>
+											{/if}
+										</Sidebar.MenuButton>
+									</a>
+								{/if}
+							{/snippet}
+						</Collapsible.Trigger>
+						<Collapsible.Content>
+							{#if mainItem.items}
+								<Sidebar.MenuSub>
+									{#each mainItem.items as subItem (subItem.title)}
+										<Sidebar.MenuSubItem>
+											<Sidebar.MenuSubButton>
+												{#snippet child({ props })}
+													<a href={subItem.url} {...props}>
+														<span>{subItem.title}</span>
+													</a>
+												{/snippet}
+											</Sidebar.MenuSubButton>
+										</Sidebar.MenuSubItem>
+									{/each}
+								</Sidebar.MenuSub>
+							{/if}
+						</Collapsible.Content>
+					</Sidebar.MenuItem>
+				{/snippet}
+			</Collapsible.Root>
+		{/each}
+	</Sidebar.Menu>
+</Sidebar.Group>
