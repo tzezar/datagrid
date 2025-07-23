b7aa0ab9ae3c1d65498d7e227178eb59552563cf
Sebastian Drozd <163430081+tzezar@users.noreply.github.com>
2025-07-23 17:43:12 +0200

    feat(): reduce generated data

--- sites/official/src/routes/enhanced/interactive-demo/+page.ts
+++ sites/official/src/routes/enhanced/interactive-demo/+page.ts
@@ -3,6 +3,6 @@ import { generateInventoryItem } from "$lib/data-generators/generate/inventory";
 
 
 export const load = async () => {
-    const inventory = generateData(generateInventoryItem, 100_000);
+    const inventory = generateData(generateInventoryItem, 10_000);
     return { inventory };
   };
